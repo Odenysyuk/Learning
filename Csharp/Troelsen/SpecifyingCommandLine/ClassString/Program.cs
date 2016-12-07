4061f7e1a85732519fa8c643ac6798abcdbc5229
Olena Denysiuk <ovdenysyuk@gmail.com>
2016-12-08 00:10:50 +0200

    Add example with book Troelsen

--- Csharp/Troelsen/SpecifyingCommandLine/ClassString/Program.cs
+++ Csharp/Troelsen/SpecifyingCommandLine/ClassString/Program.cs
@@ -11,7 +11,43 @@ namespace ClassString
         static void Main(string[] args)
         {
             String line = "This is string";
-            Console.WriteLine("Lenght {0}", line.Length); 
+            Console.WriteLine("Lenght {0}", line.Length);
+            Console.WriteLine("Compare string {0}", line.CompareTo("string"));
+            BasicStringFunctionality();
+            StringEquality();
+
+
+        }
+
+        static void BasicStringFunctionality()
+        {
+            Console.WriteLine("=> Basic String functionality:");
+            string firstName = "Freddy";
+            Console.WriteLine("Value of firstName: {0}", firstName);
+            Console.WriteLine("firstName has {0} characters.", firstName.Length);
+            Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
+            Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
+            Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains("y"));
+            Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
+            Console.WriteLine();
+        }
+
+        static void StringEquality()
+        {
+            Console.WriteLine("=> String equality:"); string s1 = "Hello!"; string s2 = "Yo!"; Console.WriteLine("s1 = {0}", s1); Console.WriteLine("s2 = {0}", s2); Console.WriteLine();
+            // Test these strings for equality.   
+            Console.WriteLine("s1 == s2: {0}", s1 == s2);
+            Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!");
+            Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+            Console.WriteLine("s1 == hello!: {0}", s1 == "hello!");
+            Console.WriteLine("s1.Equals(s2): {0}", s1.Equals(s2));
+            Console.WriteLine("Yo.Equals(s2): {0}", "Yo!".Equals(s2));
+            Console.WriteLine();
         }
     }
+
+
 }
+
+
+   
---
+++ Csharp/Troelsen/SpecifyingCommandLine/StringBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder("Hello");
+            StringBuilder str1 = new StringBuilder("Hello");
+            Console.WriteLine("Equals between str and str1 {0}", str == str1);
+            Console.WriteLine("Hello == Hello {0}", "Hello" == "Hello");
+
+        }
+    }
+}
