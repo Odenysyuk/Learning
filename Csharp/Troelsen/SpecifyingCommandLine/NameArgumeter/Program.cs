aaf4f6eb3a0d6e469228a6b9915da11846432ed6
Olena Denysiuk <ovdenysyuk@gmail.com>
2016-12-10 23:31:35 +0200

    Name argument

--- Csharp/Troelsen/SpecifyingCommandLine/NameArgumeter/Program.cs
+++ Csharp/Troelsen/SpecifyingCommandLine/NameArgumeter/Program.cs
@@ -11,6 +11,27 @@ namespace NameArgumeter
         static void Main(string[] args)
         {
 
+            DisplayFancyMessage(textColor:ConsoleColor.DarkRed, message:"New text", backgroundColor:ConsoleColor.Magenta);
+
+            Console.WriteLine("Named arguments. Step 2");
+
+            DisplayFancyMessage(message: "Step 2", textColor: ConsoleColor.Yellow, backgroundColor: ConsoleColor.DarkRed);
+
+        }
+
+        static void DisplayFancyMessage(ConsoleColor textColor, ConsoleColor backgroundColor, string message)
+        {   // Store old colors to restore after message is printed.    
+            ConsoleColor oldTextColor = Console.ForegroundColor;
+            ConsoleColor oldbackgroundColor = Console.BackgroundColor;
+
+            // Set new colors and print message.   
+            Console.ForegroundColor = textColor;
+            Console.BackgroundColor = backgroundColor; 
+
+            Console.WriteLine(message);
+
+            // Restore previous colors.    
+            Console.ForegroundColor = oldTextColor;
+            Console.BackgroundColor = oldbackgroundColor; } 
         }
     }
-}
