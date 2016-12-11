8645866db841704eb1fa00559351d3d9f422eb55
Olena Denysiuk <ovdenysyuk@gmail.com>
2016-12-11 17:53:16 +0200

    Nulllable type

---
+++ Csharp/Troelsen/SpecifyingCommandLine/NullableTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //bool myBool = null; errror, because Boolean isn't reference type
+            Console.WriteLine();
+
+            bool? myBool = null;
+            Console.WriteLine("my Bool = {0}", myBool);
+
+            Nullable<bool> nullableInt = null;
+            Console.WriteLine("my Bool != null =>{0}", nullableInt.HasValue);
+            Console.WriteLine("my Bool != null => {0}", nullableInt != null);
+
+            Console.WriteLine("Operator ??");
+            nullableInt = nullableInt ?? false;
+            Console.WriteLine("my Bool = {0}", nullableInt);
+            Console.WriteLine("my Bool != null => {0}", nullableInt != null);
+
+            Console.WriteLine("Operator ??");
+            nullableInt = nullableInt ?? true;
+            Console.WriteLine("my Bool = {0}", nullableInt);
+            Console.WriteLine("my Bool != null => {0}", nullableInt != null);
+
+
+
+
+        }
+    }
+}
---
+++ Csharp/Troelsen/SpecifyingCommandLine/RefTypeValTypeParams/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefTypeValTypeParams
+{
+    class Person
+    {
+        public string personName;
+        public int personAge;
+
+        // Constructors.   
+        public Person(string name, int age)
+        {
+            personName = name;
+            personAge = age;
+        }
+
+        public Person(){} 
+
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}", personName, personAge);
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Passing Person object by value *****");
+            Person fred = new Person("Fred", 12);
+            Console.WriteLine("\nBefore by value call, Person is:");
+            fred.Display();
+
+            SendAPersonByValue(ref fred);
+            Console.WriteLine("\nAfter by value call, Person is:");
+            fred.Display();
+            Console.ReadLine();
+        }
+
+        static void SendAPersonByValue(ref Person p)
+        {  // Change the age of "p"?   
+            p.personAge = 99;
+
+            // Will the caller see this reassignment?   
+            p = new Person("Nikki", 99);
+            p.personAge = 199;
+        } 
+
+    }
+
+    }
+
+    
