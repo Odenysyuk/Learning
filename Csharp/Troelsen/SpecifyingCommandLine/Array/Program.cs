64aa09452dc221655a5d725349e59519e53bff39
Olena Denysiuk <ovdenysyuk@gmail.com>
2016-12-11 09:42:27 +0200

    Implicity type local array

--- Csharp/Troelsen/SpecifyingCommandLine/Array/Program.cs
+++ Csharp/Troelsen/SpecifyingCommandLine/Array/Program.cs
@@ -11,6 +11,7 @@ namespace Array
         static void Main(string[] args)
         {
             SimpleArray();
+            ImplicityTypeArray();
 
         }
 
@@ -30,8 +31,25 @@ namespace Array
             {
                 Console.WriteLine("a[{0}] = {1}", i, array1[i]); 
             }
-         }
 
+            string[] array2 = { "red", "blue", "black", "pinck" };
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine("a[{0}] = {1}", i, array1[i]);
+            }
+        }
+
+        static void ImplicityTypeArray()
+        {
+            var a = new[] { 1, 2, 3, 4, 5 };//int[]
+            Console.WriteLine("a is {0}", a.ToString());
+
+            var b = new[] { 1, 2, 3.0, 4, 5 };//double[]
+            Console.WriteLine("b is {0}", b.ToString());
+
+            var mix = new[] { (object)1, (object)"hello", (object)1.3 };
+            Console.WriteLine("mix is {0}", mix.ToString());
+        }
 
     }
 }
