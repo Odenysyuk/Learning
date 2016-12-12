710ed2fb8d23ee7d3185b87abde12a62533502cc
Olena Denysiuk <ovdenysyuk@gmail.com>
2016-12-12 23:51:06 +0200

    Overides objects

--- Csharp/Troelsen/SpecifyingCommandLine/ObjectOverrides/Program.cs
+++ Csharp/Troelsen/SpecifyingCommandLine/ObjectOverrides/Program.cs
@@ -15,15 +15,38 @@ namespace ObjectOverrides
         public Person(string fName, string lName, int personAge)
         { FirstName = fName; LastName = lName; Age = personAge; }
         public Person() { }
-    }
 
+        public override string ToString()
+        {
+            string myState;
+            myState = string.Format("[First Name: {0}; Last Name: {1}; Age: {2}]", FirstName, LastName, Age);
+            return myState;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person && obj != null)
+            {
+                Person temp;
+                temp = (Person)obj;
+                if (temp.FirstName == this.FirstName && temp.LastName == this.LastName && temp.Age == this.Age)
+                { return true; }
+                else
+                {
+                    return false;
+                }
+            }
+          return false;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("=> System.Object");
 
-            Person p1= new Person();
+            Person p1= new Person() {FirstName = "Smith", LastName = "Tom", Age = 15 };
             Console.WriteLine("ToString(): {0}", p1.ToString());
             Console.WriteLine("Equals(): {0}", p1.Equals(new Person()));
             Console.WriteLine("GetHashCode(): {0}", p1.GetHashCode());
