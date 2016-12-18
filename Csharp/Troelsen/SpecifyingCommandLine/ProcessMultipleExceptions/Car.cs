e8c0c43d63a75b104e8693721df5caf6686e063e
Olena Denysiuk <ovdenysyuk@gmail.com>
2016-12-18 16:24:48 +0200

    ProcessMuilipleExceptions

---
+++ Csharp/Troelsen/SpecifyingCommandLine/ProcessMultipleExceptions/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExceptions
+{
+    class Car
+    {
+        public const int MaxSpeed = 100;
+
+        // Car properties.   
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+        private Radio theMusicBox = new Radio();
+
+        // Constructors.   
+        public Car() { }
+        public Car(string name, int speed)
+        { CurrentSpeed = speed; PetName = name; }
+
+        public void CrankTunes(bool state)
+        {     // Delegate request to inner object.   
+            theMusicBox.TurnOn(state);
+        }
+
+        // See if Car has overheated.  
+        public void Accelerate(int delta)
+        {
+            if (delta < 0)
+                throw new ArgumentOutOfRangeException("delta", "Speed must be greater than zero!");
+
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;        // carIsDead = true;
+                                             // Use the "throw" keyword to raise an exception.       
+                    CarIsDeadException ex = 
+                        new CarIsDeadException(string.Format("{0} has overheated!", PetName), 
+                                               "You have a lead foot", DateTime.Now);
+                    ex.HelpLink = @"http://www.CarsRUs.com";
+
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ Csharp/Troelsen/SpecifyingCommandLine/ProcessMultipleExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Handling Multiple Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                // Trip Arg out of range exception.     
+                myCar.Accelerate(-10);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine(); 
+          }
+    }
+}
