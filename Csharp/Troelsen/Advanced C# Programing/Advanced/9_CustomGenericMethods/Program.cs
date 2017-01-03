b833d6fc6a1426409d6b9962bf3f142335a97315
Olena Denysiuk <ovdenysyuk@gmail.com>
2017-01-03 23:38:01 +0200

    Work with custom generic class

---
+++ Csharp/Troelsen/Advanced C# Programing/Advanced/9_CustomGenericMethods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_CustomGenericMethods
+{
+    struct Point<T> where T : struct
+    {
+        T X { get; set; }
+        T Y { get; set; }
+
+
+        public Point(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}, {1}]", X, Y);
+        }
+
+        public void ResetPoint()
+        {
+            X = Y = default(T);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> p = new Point<int>(10, 10);
+
+            Point<double> d = new Point<double>(10.5, 10.5);
+
+        }
+    }
+}
