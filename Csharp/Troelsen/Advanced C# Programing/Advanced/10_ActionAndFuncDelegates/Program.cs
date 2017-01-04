43fb08d43dfda2e2cd7237875aad6967f45ee465
Olena Denysiuk <ovdenysyuk@gmail.com>
2017-01-04 22:18:07 +0200

    Working with generic delegate, such as Func<> and Action<>

---
+++ Csharp/Troelsen/Advanced C# Programing/Advanced/10_ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ActionAndFuncDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Action<> delegate, can have until 16 params");
+
+            Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
+            actionTarget.Invoke("Action message!", ConsoleColor.Yellow, 5);
+
+            Console.WriteLine();
+            Console.WriteLine("Working with Func<>, this delegate retun value. Func is like Action, take up 16 arguments and custom retun value");
+
+            Func<int, float, float> funcTarget = new Func<int, float, float>(Add);
+            float res = funcTarget.Invoke(40, 5.5f);
+            Console.WriteLine("40+ 5.5 = {0}", res);
+
+            Func<int, float, string> funcTarget2 = new Func<int, float, string>(SumToString);
+            string sum = funcTarget2(90, 300f);
+            Console.WriteLine(sum);
+
+        }
+        static void DisplayMessage(string msg, ConsoleColor txtColor,  int printCount)
+        {
+            ConsoleColor previous = Console.ForegroundColor;
+            Console.ForegroundColor = txtColor;
+            for (int i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(msg);
+            }
+
+            Console.ForegroundColor = previous;
+           
+        }
+
+        static float Add(int x, float y)
+        {
+            return x + y;
+        }
+
+        static string SumToString(int x, float y)
+        {
+            return (x + y).ToString();
+        }
+    }
+}
--- Csharp/Troelsen/Advanced C# Programing/Advanced/10_GenericDelegate/Program.cs
+++ Csharp/Troelsen/Advanced C# Programing/Advanced/10_GenericDelegate/Program.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace _10_GenericDelegate
 {
+    public delegate void MyGenericDelegate<T>(T arg);
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine(" = > Generic Delegate");
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            strTarget("Generic delegete type - > string");
+
+
+            MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+            intTarget(15);
+
+
         }
+
+        static void StringTarget(string arg) { Console.WriteLine("arg in uppercase is: {0}", arg.ToUpper()); }
+
+        static void IntTarget(int arg) { Console.WriteLine("++arg is: {0}", ++arg); }
     }
 }
